82ecefdb09666375ff6ed0ed3b7bc1cf346f1c76
JackVarney <jack.varney@iosh.com>
2018-05-07 23:26:07 +0100

    added github button because obviously im a proffessional

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 
 import Keyboard from './containers/Keyboard';
 import KeyboardProvider from './state/KeyboardProvider';
+import GitHubButton from './components/GitHubButton';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="app">
+        <GitHubButton />
         <h1 className="title">Jacks awesome keyboard</h1>
         <h1 className="mobile-message">Rotate me!</h1>
         <KeyboardProvider>
---
+++ src/components/GitHubButton.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './GitHubButton.css';
+
+export default props => (
+  <a className="github-button" href="https://github.com/JackDanielJosephVarney/keyboard-site" target="_blank" rel="noopener noreferrer">
+    <img
+      className="github-button-image"
+      height="16"
+      width="16"
+      src="https://cdn.jsdelivr.net/npm/simple-icons@1.5.0/icons/github.svg"
+      alt="GitHub Logo"
+    />Github
+  </a>
+);
