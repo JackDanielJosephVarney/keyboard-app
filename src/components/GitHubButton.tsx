6e5d065a4bf78a477e884a92e9f2fa6ae71779de
Jack Varney <32823543+JackDanielJosephVarney@users.noreply.github.com>
2018-05-08 01:26:56 +0100

    Update GitHubButton.tsx

--- src/components/GitHubButton.tsx
+++ src/components/GitHubButton.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './GitHubButton.css';
 
 export default props => (
-  <a className="github-button" href="https://github.com/JackDanielJosephVarney/keyboard-site" target="_blank" rel="noopener noreferrer">
+  <a className="github-button" href="https://github.com/JackDanielJosephVarney/keyboard-app" target="_blank" rel="noopener noreferrer">
     <img
       className="github-button-image"
       height="16"
